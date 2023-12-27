9969276f032cec1b7597e163e583b080cb44704d
Anton Eliseev <anton_saintp@mail.ru>
2023-12-27 20:48:19 +0300

    classnames

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import "./styles/index.scss";
 import { Suspense, useContext, useState } from "react";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Change theme</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
