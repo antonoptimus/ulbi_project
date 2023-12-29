ed69be34ee1c847fdc8bb0931e715c26169e6eb4
Anton Eliseev <anton_saintp@mail.ru>
2023-12-29 19:37:48 +0300

    svg loader, file loader, button ui kit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { classNames } from "shared/config/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
-import AppLink, { AppLinkTheme } from "./AppLink/AppLink";
+import AppLink, { AppLinkTheme } from "../../../shared/config/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/config/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -11,6 +12,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [])}>
+      <ThemeSwitcher/>
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.RED} to={"/"} className={cls.mainLink}>Main</AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>About</AppLink>
