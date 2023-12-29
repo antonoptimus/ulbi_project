0dd576164ef1391b5961b830c1a9d60ec56255b4
Anton Eliseev <anton_saintp@mail.ru>
2023-12-29 15:13:19 +0300

    navbar and AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,16 +6,16 @@ import { Link, Route, Routes } from "react-router-dom";
 import { useTheme } from "./providers/ThemeProvider";
 import "./styles/index.scss";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>Change theme</button>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
+      <Navbar/>
       <AppRouter />
+      <button onClick={toggleTheme}>Change theme</button>
     </div>
   );
 };
