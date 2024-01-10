f11ae5cb26c8e3ce07ebc1fa010b7ad2a4bd8979
Anton Eliseev <anton_saintp@mail.ru>
2024-01-10 14:25:07 +0300

    add babel, extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,10 +5,29 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-
   const svgLoader = {
     test: /\.svg$/,
     use: ["@svgr/webpack"],
+  };
+
+  const babelLoader = {
+    test: /\.(?:js|jsx|ts|tsx|mjs|cjs)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [["@babel/preset-env", { targets: "defaults" }]],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["en", "ru"],
+              keyAsDefaultValue: true
+            },
+          ],
+        ],
+      },
+    },
   };
 
   const cssLoader = {
@@ -45,5 +64,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
